47e938a1f41425e503a1baaaef87cb2ca2a08675
drfisherluc <94931118+drfisherluc@users.noreply.github.com>
2022-03-16 11:00:43 -0400

    Added exception handling to rest controller

--- src/main/java/com/example/lumed2crfqmiddleofficeservice/controller/RulesController.java
+++ src/main/java/com/example/lumed2crfqmiddleofficeservice/controller/RulesController.java
@@ -3,7 +3,10 @@ package com.example.lumed2crfqmiddleofficeservice.controller;
 
 import com.example.lumed2crfqmiddleofficeservice.model.WholeSectorDefinition;
 import com.example.lumed2crfqmiddleofficeservice.service.RulesService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +20,21 @@ public class RulesController {
     RulesService rs;
 
     @GetMapping("/wholerules")
-    public List<WholeSectorDefinition> getRules(@RequestParam(value = "sector", defaultValue = "ALL") String sector) {
-        if (sector.equals("ALL"))
-            return rs.getWholeRules();
-        else
-            return rs.getWholeRules(sector);
+    public ResponseEntity<String> getRules(@RequestParam(value = "sector", defaultValue = "ALL") String sector) {
+        try {
+            List<WholeSectorDefinition> wholeSectorDefinitionList = null;
+            if (sector.equals("ALL"))
+                wholeSectorDefinitionList =  rs.getWholeRules();
+            else
+                wholeSectorDefinitionList = rs.getWholeRules(sector);
+
+            String wsdStr = new Gson().toJson(wholeSectorDefinitionList);
+
+            return ResponseEntity.ok().body(wsdStr);
+        }
+        catch (Exception x) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(x.getMessage());
+        }
     }
 
 
